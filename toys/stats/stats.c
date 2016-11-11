7d11f30160dbdb96158c50036caec66963ce02ba
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-11-11 21:51:07 +0000

    create barebones example

--- toys/stats/stats.c
+++ toys/stats/stats.c
@@ -1,26 +1,11 @@
 #include <stdlib.h>
 #include <jemalloc/jemalloc.h>
 
-void
-do_something(size_t i)
-{
-
-        // Leak some memory.
-        malloc(i * 100);
-}
 
 int
-main(int argc, char **argv)
-{
-        size_t i;
+main(int argc, char **argv) {
+  jemalloc_malloc_stats_print(NULL, NULL, NULL);
 
-        for (i = 0; i < 1000; i++) {
-                do_something(i);
-        }
-
-        // Dump allocator statistics to stderr.
-        jemalloc_malloc_stats_print(NULL, NULL, NULL);
-
-        return (0);
+  return 0;
 }
 
