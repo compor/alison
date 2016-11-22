f7c5488b78b9778141eda8e31b96dbc1eec20381
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-11-22 19:02:46 +0000

    force allocation of a large chunk

--- toys/stats/s02.c
+++ toys/stats/s02.c
@@ -4,8 +4,9 @@
 
 int
 main(int argc, char **argv) {
-  jemalloc_malloc(sizeof(char) * 10);
-  jemalloc_malloc(sizeof(char) * 10);
+  jemalloc_malloc(sizeof(char) * 16 * 1024);
+
+  jemalloc_malloc_stats_print(NULL, NULL, NULL);
 
   return 0;
 }
