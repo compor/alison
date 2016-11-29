d58b3b648f8bb73c94e9faf59f15b2b4be3d5552
Chris Vassiladiotis <cvassiladiotis@gmail.com>
2016-11-29 14:13:20 +0000

    add a simple de/alloc pattern

--- toys/cppalloc/c01.cpp
+++ toys/cppalloc/c01.cpp
@@ -1,8 +1,27 @@
-#include <stdlib.h>
+
+class Complex {
+public:
+  Complex(double r, double i): m_r(r), m_i(i) {}
+
+private:
+  double m_r, m_i;
+};
+
+
+const int N = 1000;
 
 
 int
 main(int argc, char **argv) {
+  Complex *a[N];
+
+  for (int i = 0; i < 50000; i++) {
+    for (int j = 0; j < N; j++)
+      a[j] = new Complex(i, j);
+
+    for (int j = 0; j < N; j++)
+      delete a[j];
+  }
 
   return 0;
 }
